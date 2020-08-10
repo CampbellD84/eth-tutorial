cf819041dc6ad646ba8f96efacf6e6704cccb175
Douglas Campbell <dacampbelltbone@gmail.com>
2020-08-10 02:08:45 -0400

    Connect contract to React using Web3.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { simpleStorageAbi } from "./abis/abis.js";
+import Web3 from "web3";
+
+const web3 = new Web3(Web3.givenProvider);
+const contractAddr = "0xb28a4F1469637d1C270ceFF3C9583ED8F6C2C4dA";
+const SimpleContract = new web3.eth.Contract(simpleStorageAbi, contractAddr);
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const [getNumber, setGetNumber] = useState(0);
+
+  const handleGet = async (e) => {
+    e.preventDefault();
+    const result = await SimpleContract.methods.get().call();
+    setGetNumber(result);
+  };
+
+  const handleSet = async (e) => {
+    e.preventDefault();
+    const accounts = await window.ethereum.enable();
+    const account = accounts[0];
+    const gas = await SimpleContract.methods.set(number).estimateGas();
+    const result = await SimpleContract.methods
+      .set(number)
+      .send({ from: account, gas });
+    console.log(result);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSet}>
+          <label>
+            Set Number:
+            <input
+              type="text"
+              name="name"
+              value={number}
+              onChange={(e) => setNumber(e.target.value)}
+            />
+          </label>
+          <input type="submit" value="Set Number" />
+        </form>
+        <br />
+        <button onClick={handleGet} type="button">
+          Get Number
+        </button>
+        {getNumber}
       </header>
     </div>
   );
